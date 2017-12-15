60c74199f035cc8781ba5d4f744b2909d0a5ea12
mikedev9000 <michael1.cis@gmail.com>
2017-12-15 17:31:06 -0500

    connected charts... so easy

--- src/components/Scratch.js
+++ src/components/Scratch.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
-const d = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100}
-];
-
 
 function sinHelper(progress, min, max) {
   var height = max - min;
@@ -26,19 +16,34 @@ const data = [];
 for(var i = 0; i < 100; i++) {
   data.push({ name: i,
     valueA: sinHelperWithNoise(i / 100, 0, 100, 10),
-    valueB: sinHelperWithNoise(i / 100, -100, 200, 100)
+    valueB: i % 8 !== 0 && sinHelperWithNoise(i / 100, -100, 200, 100),
+    valueC: i % 8 !== 0 && sinHelperWithNoise(i / 100, -100, 200, 1000)
   });
 }
 
 export default () => (
-  <LineChart width={600} height={300} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-    <XAxis dataKey="name"/>
-    <YAxis/>
-    <CartesianGrid strokeDasharray="3 3"/>
-    <Tooltip/>
-    <Legend />
-    <Line type="monotone" dataKey="valueA" stroke="#8884d8" activeDot={{r: 8}}/>
-    <Line type="monotone" dataKey="valueB" stroke="#bbbbbb" activeDot={{r: 8}}/>
-  </LineChart>
+  <div>
+    <LineChart width={600} height={300} data={data}
+              margin={{top: 5, right: 30, left: 20, bottom: 5}}
+              syncId="foo">
+      <XAxis dataKey="name"/>
+      <YAxis/>
+      <CartesianGrid strokeDasharray="3 3"/>
+      <Tooltip/>
+      <Legend />
+      <Line connectNulls={true} type="monotone" dataKey="valueA" stroke="#8884d8" activeDot={{r: 8}}/>
+      <Line connectNulls={true} type="monotone" dataKey="valueB" stroke="#bbbbbb" activeDot={{r: 8}}/>
+    </LineChart>
+    <LineChart width={600} height={300} data={data}
+              margin={{top: 5, right: 30, left: 20, bottom: 5}}
+              syncId="foo">
+      <XAxis dataKey="name"/>
+      <YAxis/>
+      <CartesianGrid strokeDasharray="3 3"/>
+      <Tooltip/>
+      <Legend />
+      <Line connectNulls={true} type="monotone" dataKey="valueC" stroke="#8884d8" activeDot={{r: 8}}/>
+      <Line connectNulls={true} type="monotone" dataKey="valueB" stroke="#bbbbbb" activeDot={{r: 8}}/>
+    </LineChart>
+  </div>
 );
