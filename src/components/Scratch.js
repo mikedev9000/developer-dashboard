910e8d206a7074603f46164bed6a4bb834d5bbf7
mikedev9000 <michael1.cis@gmail.com>
2017-12-16 14:43:04 -0500

    oops, add graphs back

--- src/components/Scratch.js
+++ src/components/Scratch.js
@@ -16,6 +16,27 @@ class Scratch extends React.Component {
   render() {
     return (
       <div>
+        <LineChart width={400} height={300} data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          syncId="foo">
+          <XAxis dataKey="timestamp" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line connectNulls type="monotone" dataKey="temperature" stroke="#8884d8" />
+        </LineChart>
+        <LineChart width={400} height={300} data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          syncId="foo">
+          <XAxis dataKey="timestamp" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line connectNulls type="monotone" dataKey="foo" stroke="#8884d8" />
+          <Line connectNulls type="monotone" dataKey="bar" stroke="#bbbbbb" />
+        </LineChart>
         <div style={{ height: 115 }} className="ag-theme-fresh">
           <AgGridReact
             // properties
